42ef73689e5a3b0870c2a6558816322ba255037a
RyotaroIsoyama <madao329@gmail.com>
2020-12-16 17:03:42 +0900

    done fetch google events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'events#new'
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   get "time_schedules/list",to: "time_schedules#list"
+  get "events/token",to: "events#token"
+  post "events/callback",to: "events#callback"
+  get "calendar/index", to:"calendar#index"
+  get "oauth2callback", to:"calendar#callback"
+
   resources :events
   resources :time_schedules
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
