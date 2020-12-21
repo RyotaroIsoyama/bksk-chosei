e99076a48f71136b34f3a769f8c3ff112a2634b2
RyotaroIsoyama <madao329@gmail.com>
2020-12-21 23:51:41 +0900

    save

--- app/controllers/time_schedules_controller.rb
+++ app/controllers/time_schedules_controller.rb
@@ -28,6 +28,7 @@ class TimeSchedulesController < ApplicationController
   def list
     session[:url] = CREDENTIALS_URL
     @event = Event.where(hashforurl: params[:h]).first
+    @user = current_user
     days = (@event.enddate - @event.stdate).to_i + 1
     session[:startTime] = @event.stdate.strftime("%Y%m%d %H:%M:%S")
     session[:endTime] = (@event.enddate + 1).strftime("%Y%m%d %H:%M:%S")
@@ -39,6 +40,6 @@ class TimeSchedulesController < ApplicationController
 
   private
     def time_schedule_params
-      params.require(:form_time_schedule_collection).permit(time_schedules_attributes: [:username,:event_id,:eventday,:t0,:t1,:t2,:t3,:t4,:t5,:t6,:t7,:t8,:t9,:t10,:t11,:t12,:t13,:t14,:t15,:t16,:t17,:t18,:t19,:t20,:t21,:t22,:t23,:t24,:t25,:t26,:t27,:t28,:t29,:t30,:t31,:t32,:t33,:t34,:t35,:t36,:t37,:t38,:t39,:t40,:t41,:t42,:t43,:t44,:t45,:t46,:t47])
+      params.require(:form_time_schedule_collection).permit(:username,:user_id,:event_id,time_schedules_attributes: [:eventday,:t0,:t1,:t2,:t3,:t4,:t5,:t6,:t7,:t8,:t9,:t10,:t11,:t12,:t13,:t14,:t15,:t16,:t17,:t18,:t19,:t20,:t21,:t22,:t23,:t24,:t25,:t26,:t27,:t28,:t29,:t30,:t31,:t32,:t33,:t34,:t35,:t36,:t37,:t38,:t39,:t40,:t41,:t42,:t43,:t44,:t45,:t46,:t47])
     end
 end
